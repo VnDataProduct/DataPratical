c675130745b2c804337aee4bf72228d445adfc8b
pnthang01 <pnthang01@gmail.com>
2023-01-28 14:31:39 +0700

    Update http 002

--- http-log-handling-02/src/main/java/org/vndataproduct/httplog002/MainStarter.java
+++ http-log-handling-02/src/main/java/org/vndataproduct/httplog002/MainStarter.java
@@ -1,15 +1,34 @@
-package org.vndataproduct.httplog001;
+package org.vndataproduct.httplog002;
 
+import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.file.FileSystem;
 import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.redis.client.Redis;
+import io.vertx.redis.client.RedisAPI;
+import io.vertx.redis.client.RedisConnection;
+import io.vertx.redis.client.RedisOptions;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainStarter {
+
+   RedisAPI redisAPI = null;
+
+   public void setRedisAPI(RedisAPI redisAPI) {
+      this.redisAPI = redisAPI;
+   }
+
+   DateFormat hDF = new SimpleDateFormat("yyyy-MM-dd-HH");
+   DateFormat fileDF = new SimpleDateFormat("'log_'yyyy-MM-dd-HH'.tsv'");
 
    public void handleRequest(RoutingContext context) {
       //Get response data from context
@@ -19,9 +38,45 @@ public class MainStarter {
          //This will get body as Buffer then cast it to JsonObject
          JsonObject dataBody = context.body().asJsonObject();
          System.out.println("Data Received: " + dataBody.toString());
+         //If we let above methods use their own time, it is possible that data will be discrepancy
+         Date receiveTime = new Date();
+         this.sumToRedis(receiveTime, dataBody);
+         this.writeLog(context.vertx(), receiveTime, dataBody);
       } catch (Exception e) {
          e.printStackTrace();
       }
+   }
+
+   private void sumToRedis(Date receiveTime, JsonObject row) {
+      boolean isSuccess = row.getString("result").equals("success");
+      String hKey = hDF.format(receiveTime);
+      if(isSuccess) {
+         this.redisAPI.hincrby(hKey, "success_device:" + row.getString("device"), "1");
+         this.redisAPI.hincrby(hKey, "success_browser:" + row.getString("browser"), "1");
+         this.redisAPI.hincrby(hKey, "success_time", row.getInteger("duration") + "");
+         if(row.getString("accountId") != null)
+            this.redisAPI.hincrby(hKey, "success_reg_user", "1");
+      } else {
+         this.redisAPI.hincrby(hKey, "error_device:" + row.getString("device"), "1");
+         this.redisAPI.hincrby(hKey, "error_browser:" + row.getString("browser"), "1");
+         this.redisAPI.hincrby(hKey, "error_time", row.getInteger("duration") + "");
+         this.redisAPI.hincrby(hKey, "error_cause", row.getString("message"));
+         if(row.getString("accountId") != null)
+            this.redisAPI.hincrby(hKey, "error_reg_user", "1");
+      }
+   }
+
+   //Since FileSystem instance need to be created from Vertx instance, we have to put in method
+   private void writeLog(Vertx vertx, Date receiveTime, JsonObject row) {
+      FileSystem fs = vertx.fileSystem();
+      StringBuilder sb = new StringBuilder();
+      sb.append(row.getString("device")).append("\t");
+      sb.append(row.getString("browser")).append("\t");
+      sb.append(row.getString("result")).append("\t");
+      sb.append(row.getString("message")).append("\t");
+      sb.append(row.getInteger("duration")).append("\t");
+      sb.append(row.getString("accountId", ""));
+      fs.writeFile(fileDF.format(receiveTime), Buffer.buffer(sb.toString()));
    }
 
    public static void main(String[] args) {
@@ -34,8 +89,15 @@ public class MainStarter {
       router.post("/accept_tracking")
             .handler(BodyHandler.create())
             .handler(handlerObj::handleRequest);
-      //
-      HttpServer httpServer = vertx.createHttpServer();
-      httpServer.requestHandler(router).listen(8080);
+      //Initialize Redis instance
+      RedisOptions options = new RedisOptions()
+            .setConnectionString("redis://localhost:6379");
+      Future<RedisConnection> connect = Redis.createClient(vertx, options).connect();
+      connect.onSuccess(res -> {
+         handlerObj.setRedisAPI(RedisAPI.api(res));
+         //We need to make sure Redis connection is established successfully before init the HTTP service
+         HttpServer httpServer = vertx.createHttpServer();
+         httpServer.requestHandler(router).listen(8080);
+      });
    }
 }
